6a6be1800cef935122bb5c3d873e21202f0ab299
markgregg <gregg.mark@gmail.com>
2023-10-25 20:28:18 +0100

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  //base: "multi-select-example",
   plugins: [
     react(),
     eslintPlugin(),
