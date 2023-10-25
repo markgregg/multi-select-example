00837fb5c18e4793967ca1d615c397bbcb524de7
markgregg <gregg.mark@gmail.com>
2023-10-25 20:07:42 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,8 @@ import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [
     react(),
     eslintPlugin(),
-  ]
+  ],
 })
